f24529a4b5e4a268018ad87d88622971429e6c5f
StraightUpGruntled <braydenplumb@gmail.com>
2026-01-31 01:54:56 -0800

    Title Screen - Creation and initial setup
    -Created the title_screen.tscn scene.
    -Setup node structure, and play, options, and quit buttons.
    -Created the title_screen.gd script.
    -Connected buttons to the title screen script.
    -Update boot intro sequence to change to the title screen on completion.

--- game/game_scenes/boot_intro_sequence/boot_intro_sequence.gd
+++ game/game_scenes/boot_intro_sequence/boot_intro_sequence.gd
@@ -1,10 +1,13 @@
 class_name BootIntroSequence
-extends Node2D
+extends Control
 
 @export var intro_anim_name: String = "intro"
+@export var title_screen_scene: PackedScene
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _ready() -> void:
 	animation_player.play(intro_anim_name)
+	await animation_player.animation_finished
+	get_tree().change_scene_to_packed(title_screen_scene)
---
+++ game/game_scenes/title_screen/title_screen.gd
@@ -0,0 +1,17 @@
+class_name TitleScreen
+extends Control
+
+
+
+# Menu Button Signal Connections #
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with change scene to gameplay scene.
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with settings menu opening.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
